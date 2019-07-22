568aa2a68adc1c7abd88746f9f26c14601158ebb
github username <github email address>
2019-07-22 18:13:10 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,13 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
-  private
+  before_validation :make_title_case
 
+  # before_save :email_author_about_post
+
+  private
+#title.split = splits a string into an array of individual words
+#if any of these words first letter (w[0]) is not an upcase, then return the "Title must be in the title case"
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
